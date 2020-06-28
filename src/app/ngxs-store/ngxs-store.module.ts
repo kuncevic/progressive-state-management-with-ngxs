3b10bb8f779047b9a050330edf36eda5a7a3fd9b
kuncevic <kncvch@gmail.com>
2020-06-28 13:46:55 +1000

    add menu

--- src/app/ngxs-store/ngxs-store.module.ts
+++ src/app/ngxs-store/ngxs-store.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { NgxsModule } from '@ngxs/store';
 import { ChildComponent } from './child/child.component';
 import { LittleChildComponent } from './little-child/little-child.component';
@@ -13,6 +14,7 @@ import { CounterState } from './state/counter.state';
   declarations: [ChildComponent, ParentComponent, NgxsComponent, LittleChildComponent],
   imports: [
     CommonModule,
+    RouterModule,
     NgxsModule.forFeature([CounterState]),
   ],
 })
