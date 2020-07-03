fe1051c66ad2cf7d70cd174759c2ba5d0d07a958
kuncevic <kncvch@gmail.com>
2020-07-03 20:07:52 +1000

    add readonly to actons

--- src/app/ngxs-store/state/actions.ts
+++ src/app/ngxs-store/state/actions.ts
@@ -5,15 +5,15 @@ export class ResetCounter {
 
 export class UpdateValue1 {
   public static type = '[Sum] Update Value1';
-  constructor(public amount: number) {}
+  constructor(public readonly amount: number) {}
 }
 
 export class UpdateValue2 {
   public static type = '[Sum] Update Value2';
-  constructor(public amount: number) {}
+  constructor(public readonly amount: number) {}
 }
 
 export class UpdateValue3 {
   public static type = '[Sum] Update Value3';
-  constructor(public amount: number) {}
+  constructor(public readonly amount: number) {}
 }
