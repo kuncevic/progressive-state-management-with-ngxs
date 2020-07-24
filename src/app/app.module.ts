e25c863c1d21930f1fd6976ddda1cbf33807a9f3
kuncevic <kncvch@gmail.com>
2020-07-24 12:23:20 +1000

    disable logger and redux dev tools for prod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxsDataPluginModule } from '@ngxs-labs/data';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from "@ngxs/logger-plugin";
 import { NgxsModule } from '@ngxs/store';
 import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
@@ -8,8 +10,6 @@ import { AppComponent } from './app.component';
 import { NgxsDataModule } from './ngxs-data/ngxs-data.module';
 import { NgxsStoreModule } from './ngxs-store/ngxs-store.module';
 import { ReactiveServiceModule } from './reactive-service/reactive-service.module';
-import { NgxsLoggerPluginModule } from "@ngxs/logger-plugin";
-import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 
 @NgModule({
   declarations: [AppComponent],
@@ -24,8 +24,8 @@ import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
       selectorOptions: { injectContainerState: false },
     }),
     NgxsDataPluginModule.forRoot(),
-    NgxsLoggerPluginModule.forRoot(),
-    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot({ disabled: environment.production }),
+    NgxsReduxDevtoolsPluginModule.forRoot({ disabled: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent],
